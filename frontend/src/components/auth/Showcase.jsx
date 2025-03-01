bb87a69ff5a4f560ecff2bd375a25f53a6b17ca9
PV SONALI <pv.sonali.official@gmail.com>
2025-03-01 11:57:34 +0530

    completed milestone-12

---
+++ frontend/src/components/auth/Showcase.jsx
@@ -0,0 +1,9 @@
+function Showcase() {
+    return (
+      <div className='bg-gray-700'>
+        <img src="" alt="showcase 1" className='w-[90%] flex justify-center m-auto' />
+      </div>
+    )
+  }
+  
+  export default Showcase
